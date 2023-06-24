b85237068da18bb38b4d79e68e7fe70dac6207e1
Goodness Nwankwo (NDER) <126233126+NdersCode@users.noreply.github.com>
2023-06-24 09:24:45 +0100

    Latest Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'package:app_004/expense_tracker.dart';
+import 'package:app_004/expenses.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: ...,
+      home: Expenses(),
     ),
   );
 }
