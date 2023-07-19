6099acd2d3b9858e0ceeb068021648eac7d7f052
Goodness Nwankwo (NDER) <126233126+NdersCode@users.noreply.github.com>
2023-07-19 09:48:51 +0100

    Latest Commit

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -17,10 +17,15 @@ class ExpensesList extends StatelessWidget {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (context, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+        ),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
-        key: ValueKey(expenses[index]),
         child: ExpenseItem(expenses[index]),
       ),
     );
